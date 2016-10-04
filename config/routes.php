060a8056843189e85fe493056aea62f850328148
KingLoui <matzehollerbach@web.de>
2016-10-04 14:31:16 +0200

    updated routes

--- config/routes.php
+++ config/routes.php
@@ -5,24 +5,6 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
-/**
- * The default class to use for all routes
- *
- * The following route classes are supplied with CakePHP and are appropriate
- * to set as the default:
- *
- * - Route
- * - InflectedRoute
- * - DashedRoute
- *
- * If no call is made to `Router::defaultRouteClass()`, the class used is
- * `Route` (`Cake\Routing\Route\Route`)
- *
- * Note that `Route` does not do any inflections on URLs which will result in
- * inconsistently cased URLs when used with `:plugin`, `:controller` and
- * `:action` markers.
- *
- */
 Router::defaultRouteClass(DashedRoute::class);
 
 Router::scope('/', function (RouteBuilder $routes) {
@@ -43,8 +25,12 @@ Router::prefix('admin', function ($routes) {
 
 Router::connect(
     '/login',
-    ['controller' => 'Users', 'action' => 'login'],
-    ['_name' => 'login']
+    ['controller' => 'Users', 'action' => 'login']
+);
+
+Router::connect(
+    '/logout',
+    ['controller' => 'Users', 'action' => 'logout']
 );
 
 Router::connect(
@@ -52,11 +38,4 @@ Router::connect(
     ['controller' => 'Pages', 'action' => 'display', 'dashboard']
 );
 
-Router::connect('/admin/users/login', ['_name' => 'login']);
-
-
-/**
- * Load all plugin routes.  See the Plugin documentation on
- * how to customize the loading of plugin routes.
- */
 Plugin::routes();
